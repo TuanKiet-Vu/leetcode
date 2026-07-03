da1172ba182e143c197570e405a87b01fe85a828
TuanKiet-Vu <vutuankiet9737@gmail.com>
2026-07-03 11:02:03 +0700

    Implement maxArea function for water container problem

--- DSA/Two Pointers/Container with most water/solution.cpp
+++ DSA/Two Pointers/Container with most water/solution.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int maxdt = 0;
+        int i = 0 , j=height.size()-1;
+        while (i<j){
+            int curdt = (j-i) * min(height[i],height[j]);
+            maxdt = max(curdt,maxdt);
+            if (height[i]<height[j]) i++;
+            else if (height[i]>height[j]) j--;
+            else{
+                i++;
+                j--;
+            }
+        }
+        return maxdt;
+    }
+};
