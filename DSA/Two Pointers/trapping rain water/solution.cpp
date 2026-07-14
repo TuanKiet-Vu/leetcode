39e6940efc2930187f5de873684968bf35a0464f
TuanKiet-Vu <vutuankiet9737@gmail.com>
2026-07-14 14:57:27 +0700

    Implement trapping rain water solution using two pointers

--- DSA/Two Pointers/trapping rain water/solution.cpp
+++ DSA/Two Pointers/trapping rain water/solution.cpp
@@ -1,1 +1,25 @@
-
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n =  height.size();
+        if (n < 3) return 0;
+        
+        int container = 0 , maxLeft = 0, maxRight = 0;
+        int l = 0 , r = n-1;
+        
+        while(l<r){
+            if (height[l]<height[r]){
+                maxLeft = max(maxLeft,height[l]);
+                container += maxLeft - height[l];
+                l++;
+            }
+            else{
+                maxRight = max(maxRight,height[r]);
+                container += maxRight - height[r];
+                r--;
+            }
+                
+        }
+           return container;
+    }
+};
