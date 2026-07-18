d6b841b600b15a3df7540e7a5926e7b474d8d4d0
TuanKiet-Vu <vutuankiet9737@gmail.com>
2026-07-18 14:40:33 +0700

    Implement MinStack class with push, pop, top, and getMin

--- DSA/Stack/Min stack/approach_2.cpp
+++ DSA/Stack/Min stack/approach_2.cpp
@@ -1,1 +1,39 @@
+class MinStack {
+public:
+    stack<long long> st;
+    long long  curMin = INT_MAX;
+    MinStack() { }
+    
+    void push(long long value) {
+        if (st.empty()) {
+           st.push(value);
+           curMin = value;
+        }
+        else if (value >= curMin)
+           st.push(value);
+        else{
+           st.push (2*value - curMin);
+           curMin = value;
+        }
+    }
+    
+    void pop() {
+        long long t = st.top();
+        st.pop();
+        
+        if (t < curMin)
+          curMin = 2*curMin - t;
 
+    }
+    
+    int top() {
+        if (st.top() >= curMin)
+          return st.top();
+        else
+          return curMin;
+    }
+    
+    int getMin() {
+        return curMin;
+    }
+};
