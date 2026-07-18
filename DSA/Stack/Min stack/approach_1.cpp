6390a47ca24939b22cba866eabb79c927220aee7
TuanKiet-Vu <vutuankiet9737@gmail.com>
2026-07-18 14:40:55 +0700

    Implement MinStack with push, pop, top, and getMin

--- DSA/Stack/Min stack/approach_1.cpp
+++ DSA/Stack/Min stack/approach_1.cpp
@@ -1,1 +1,29 @@
+class MinStack {
+public:
+    stack<int> st;
+    stack<int> minst;
+    MinStack() { }
+    
+    void push(int value) {
+        st.push(value);
+        if (minst.empty() || value <= minst.top())
+           minst.push (value);
+        else
+           minst.push (minst.top());
+    }
+    
+    void pop() {
+        st.pop();
+        minst.pop();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return minst.top();
+    }
+};
+
 
