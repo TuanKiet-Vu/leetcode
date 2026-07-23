4a76bf730b608be61e3c6a46c3e837988afd2d72
TuanKiet-Vu <vutuankiet9737@gmail.com>
2026-07-23 15:17:25 +0700

    Add solution for largest rectangle in histogram
    Implement largest rectangle area calculation using a stack.

--- DSA/Stack/Largest rectangle in histogram/solution.cpp
+++ DSA/Stack/Largest rectangle in histogram/solution.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;
+        int n = heights.size();
+        int ans = INT_MIN;
 
+        for (int i = 0 ; i<=n ; i++){
+            int h = (i==n)? 0:heights[i];
+            while (!st.empty() && h <= heights[st.top()]){
+                int height = heights[st.top()];
+                st.pop();
+                int width = st.empty()? i:i-st.top()-1;
+                ans = max (ans,height*width);
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
