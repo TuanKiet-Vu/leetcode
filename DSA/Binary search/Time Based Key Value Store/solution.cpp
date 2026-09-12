24d9264ae65c698fc632252ffa4d7b205d0e9d9d
TuanKiet-Vu <vutuankiet9737@gmail.com>
2026-09-12 16:21:09 +0700

    Add TimeMap class with set and get methods
    Implement a time-based key-value store with set and get methods using binary search.

--- DSA/Binary search/Time Based Key Value Store/solution.cpp
+++ DSA/Binary search/Time Based Key Value Store/solution.cpp
@@ -1,1 +1,40 @@
+class TimeMap(object):
 
+
+    def __init__(self):
+        self.data = {}
+
+    def set(self, key, value, timestamp):
+        self.data.setdefault (key,[]).append((value,timestamp))
+        
+
+    def get(self, key, timestamp):
+        if key not in self.data:
+            return ""
+        
+        ans = ""
+        left = 0
+        right = len(self.data[key]) -1
+        map = self.data[key]
+
+        while left <= right:
+            mid = left + (right-left)//2
+
+
+            if map[mid][1] == timestamp:
+                return map[mid][0]
+            elif map[mid][1] < timestamp:
+                ans = map[mid][0]
+                left = mid + 1
+                
+            else:
+                right = mid - 1
+
+        return ans
+        
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
