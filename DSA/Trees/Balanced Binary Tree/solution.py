edeac45e359fc7a76c40b5254d0d350a11ec4414
TuanKiet-Vu <vutuankiet9737@gmail.com>
2026-09-21 15:50:17 +0700

    Update solution.py

--- DSA/Trees/Balanced Binary Tree/solution.py
+++ DSA/Trees/Balanced Binary Tree/solution.py
@@ -1,1 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isBalanced(self, root):
+        if root is None:
+            return True
 
+        def height( root):
+            if root is None:
+                return 0
+
+            left = height(root.left)
+            if left == -1:
+                return -1
+
+            right = height(root.right)
+            if right == -1:
+                return -1
+
+            if abs(left-right) > 1:
+                return -1
+
+            return max(left, right) + 1
+        
+        return height(root)!= -1
